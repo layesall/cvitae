becca1c6e92a712fade290e7000e5c93fa471333
Layesall <contact@layesall.com>
2022-05-03 04:35:05 +0200

    Project cloned from the [https://github.com/layesall/reactapp-starter-kit.git]

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./index.css";
+
+import App from "./components/App";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
