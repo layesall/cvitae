088b738f288e9188eb0fa53fae86eb3a5f452294
Layesall <contact@layesall.com>
2022-05-03 05:27:06 +0200

    config - cvitae data added

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./Welcome.css";
 
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
+import cvdata from "../../config/config.json"
+
 export default function Welcome() {
+
+  const [cvitae, setCvitae] = useState([]);
+
+  useEffect(() => {
+    return () => {
+      setCvitae(cvdata);
+    };
+  }, []);
+  
+  console.log(cvitae);
+
   return (
     <div className="welcome-component">
       <Header />
